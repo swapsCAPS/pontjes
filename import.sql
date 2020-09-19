1a5c03669615e6da1fe281931fba520a6051a4c8
swapsCAPS <verstegen.daan@gmail.com>
2020-09-19 12:49:03 +0200

    display stops

--- import.sql
+++ import.sql
@@ -1,4 +1,9 @@
 .mode csv
+drop table stops;
+drop table calendar_dates;
+drop table trips;
+drop table stop_times;
+drop table routes;
 .import gtfs/stops.txt stops
 .import gtfs/calendar_dates.txt calendar_dates
 .import gtfs/trips.txt trips
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate diesel;
 
-use rocket::http::{
-    RawStr,
-};
-
-use diesel::{SqliteConnection, result::QueryResult, prelude::*};
-
-use rocket_contrib::{
-    templates::Template,
-    serve::StaticFiles,
-};
-
+use diesel::{prelude::*, result::QueryResult, SqliteConnection};
+use rocket::http::RawStr;
+use rocket_contrib::{serve::StaticFiles, templates::Template};
 use std::collections::HashMap;
 
 mod schema {
     table! {
         pont_stops (stop_id) {
-            stop_name -> Text,
             stop_id -> Text,
-
+            stop_name -> Text,
         }
     }
 }
 
-use self::schema::pont_stops;
 use self::schema::pont_stops::dsl::*;
 
 #[derive(serde::Serialize, Queryable, Debug)]
@@ -40,18 +33,30 @@ pub struct Stop {
 struct PontjesDb(SqliteConnection);
 
 #[get("/")]
-fn index(conn: PontjesDb) {
+fn index(conn: PontjesDb) -> Template {
+    match pont_stops.load::<Stop>(&*conn) {
+        Ok(results) => {
+            let mut context = HashMap::new();
+            context.insert("stops", results);
+            Template::render("index", &context)
+        }
+        Err(e) => {
+            println!("Error! {}", e);
+            Template::render("error", ())
+        }
+    }
+}
 
-    let results = pont_stops.load::<Stop>(&*conn);
-    println!("results {:?}", results);
+#[get("/departures/<id>")]
+fn stop(conn: PontjesDb, id: &RawStr) {
+    println!("stop_id {}", id.as_str());
 }
 
 fn main() {
     rocket::ignite()
         .attach(PontjesDb::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, stop])
+        .mount("/public", StaticFiles::from("./public"))
         .attach(Template::fairing())
         .launch();
 }
-
-
