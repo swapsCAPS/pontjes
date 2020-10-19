3e515a0d55b2fcf186db95331197edcfd0eb8b45
swapsCAPS <verstegen.daan@gmail.com>
2020-10-19 11:27:58 +0200

    really need to make this more prod ready... :')

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+TAG=latest
 cargo build --release
 scripts/download-and-import.sh
-docker build . -t pontjes:latest
-scripts/docker-run.sh
+docker build . -t pontjes:$TAG
+scripts/docker-run.sh $TAG
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,3 +1,4 @@
+TAG=$1
 docker stop pontjes
 docker rm -f pontjes
-docker run -d -p 6376:6376 --name pontjes --restart always pontjes:1
+docker run -d -p 6376:6376 --name pontjes --restart always pontjes:$TAG
