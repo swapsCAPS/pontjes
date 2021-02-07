fdc6d4f34e797f92198849b893012571fdf25e86
swapsCAPS <verstegen.daan@gmail.com>
2021-02-07 18:03:54 +0100

    version some assets

--- public/scripts/main-v1.js
+++ public/scripts/main-v1.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker.register("/sw-v1.js");
   }
 });
